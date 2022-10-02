2762a93795c07a2dc4b878c25d5e1a262a7e77d4
b0binho <90756475+b0binho@users.noreply.github.com>
2022-10-02 21:40:07 +0200

    Add files via upload

---
+++ pomidor/script.js
@@ -0,0 +1,6 @@
+const hamburger = document.getElementById('hamburger');
+const nav__links = document.getElementById('nav__links');
+
+hamburger.addEventListener(click, () => {
+    nav__links.classList.toggle('show'):
+});
